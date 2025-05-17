29a08b10bd089b635830ba11454344f663776d6a
rywkoo <61554436+rywkoo@users.noreply.github.com>
2025-05-18 01:58:30 +0700

    Update start-h.bat

--- start-h.bat
+++ start-h.bat
@@ -2,17 +2,13 @@
 :: Change to current working directory
 cd /d "%CD%"
 
-:: Activate virtual environment
 call .venv\Scripts\activate.bat
 
-:: Run Flask app in background
 echo Starting Flask server...
 start "" /b python app.py
 
-:: Wait 10 seconds to allow server to start
 timeout /t 10 /nobreak >nul
 
-:: Open browser
 start http://127.0.0.1:5000
 
 pause
